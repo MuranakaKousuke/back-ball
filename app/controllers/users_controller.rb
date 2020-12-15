5f88f65dccb9c28bd454e08a3cd3a8370b395045
Kousuke Muranaka <muranaka_kousuke@yahoo.co.jp>
2020-12-15 09:37:51 +0900

    railsのポート番号を30001に設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def create
-    if User.create(user_params)
+    user = User.new(user_params)
+    if user.save
       render json: {results: true}
     else
       render json: {results: false}
