5fa6d3bf8b0673083dc15d4c9d821d65dff44e49
Kousuke Muranaka <muranaka_kousuke@yahoo.co.jp>
2020-12-18 16:19:17 +0900

    都道府県作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_065836) do
+ActiveRecord::Schema.define(version: 2020_12_18_070248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "prefectures", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "email", null: false
