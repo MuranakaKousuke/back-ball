4dd0be2156afa3611a86b14108ae7efd61fcb348
Kousuke Muranaka <muranaka_kousuke@yahoo.co.jp>
2020-12-18 19:03:08 +0900

    チーム編集機能を追加

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,16 +1,44 @@
 class TeamsController < ApplicationController
   def create
-    team = Team.new(team_params)
+    team = Team.new(team_create_params)
     if team.save
-      render json: {results: true}
+      render json: {
+        results: true,
+      }
     else
-      render json: {results: false}
+      render json: {
+        results: false,
+      }
+    end
+  end
+
+  def update
+    team = Team.find_by(id: params[:id])
+    if team&.update(team_update_params)
+      render json: {
+        results: true,
+        team: {
+          id: team.id,
+          name: team.name,
+          town: team.town,
+          introduction: team.introduction,
+          prefectureId: team.prefecture_id,
+        }
+      }
+    else
+      render json: {
+        results: false,
+      }
     end
   end
 
   private
   # TODO: require(:team)をするとpassword, password_confirmationが受け取れない問題を修正する
-  def team_params
+  def team_create_params
     params.permit(:email, :password, :password_confirmation)
+  end
+
+  def team_update_params
+    params.permit(:name, :prefecture_id, :town, :introduction)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :teams, only: [:create]
+  resources :teams, only: [:create, :update]
 
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
